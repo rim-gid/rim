30f5ff6ababe8fa2fc239a26ad04e1d3e0d425fe
andrey <aa.veter@gmail.com>
2011-10-24 00:20:33 +0400

    modified:   ../../urls.py 	modified:   ../../views.py

--- rimgid/views.py
+++ rimgid/views.py
@@ -79,9 +79,9 @@ def ex_list(request):
     excursion_list = Excursion.objects.all();
     return render_to_response(url, locals())
     
-def border_radius(request):
-    url = 'border-radius.html';
-    return render_to_response(url, locals())
+#def border_radius(request):
+#    url = 'border-radius.html';
+#    return render_to_response(url, locals())
 
 def get_css(request,name):
     url = "css/" + name + ".css";
